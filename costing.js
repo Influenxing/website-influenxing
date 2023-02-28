2367cf1d41474bb60bdfe892c9ce0456151294dd
Influenxing <118193122+Influenxing@users.noreply.github.com>
2023-02-28 23:22:52 +0530

    Update Website

---
+++ costing.js
@@ -0,0 +1,20 @@
+var costingText = `<div data-custom-class="body">
+<div>
+         <p class="c0"><span class="c4">Costing</span></p>
+         <p class="c0 c1"><span class="c2"></span></p>
+      </div>
+      <p class="c0"><span class="c2"><br>Commission fees are a common way for businesses to generate revenue and cover their operational expenses. This fee is usually a percentage of the transaction value and covers the cost of the business&rsquo;s operations, such as payment processing, customer service, and platform maintenance.</span></p>
+      <p class="c0 c1"><span class="c2"></span></p>
+      <p class="c0"><span class="c2">The exact commission fee charged depend on various factors, such as the industry, competition, and market conditions. For example, a business in a highly competitive industry may charge a lower commission fee than one in a less competitive industry. Additionally, market conditions can also affect the commission fee. If demand for a product or service is high, the commission fee may be higher than if demand is low.</span></p>
+      <p class="c0 c1"><span class="c2"></span></p>
+      <p class="c0"><span class="c2">Overall, a commission fee is a way for businesses to generate revenue and cover their operational expenses while providing valuable services to their customers. It is important to understand the different factors that can affect the commission fee in order to make informed decisions about pricing and operations.</span></p>
+      <p class="c0 c1"><span class="c2"></span></p>
+      <p class="c0 c1"><span class="c2"></span></p>
+</div>`;
+
+var Costing = function () {
+  var costing = {
+    text: costingText,
+  };
+  return costing;
+};
---
+++ faq.js
@@ -0,0 +1,32 @@
+var FaqText = `<div data-custom-class="body">
+<div>
+<p class="c1"><span class="c4">FAQ </span></p>
+<p class="c1 c2"><span class="c0"></span></p>
+</div>
+<p class="c1"><span><br></span><span class="c0">Do we have to pay to search and find influencers ?</span></p>
+<p class="c1"><span class="c0">No , you do not have to pay to search or find influencers. It&#39;s completely free !</span></p>
+<p class="c1 c2"><span class="c0"></span></p>
+<p class="c1"><span class="c0">What is commision fee ?</span></p>
+<p class="c1"><span class="c0">We charge a standard 2% commission fee on ever collaboration on our platform, which we use for our business operations.</span></p>
+<p class="c1 c2"><span class="c0"></span></p>
+<p class="c1"><span class="c0">Will i get a refund if i cancel a campaign?</span></p>
+<p class="c1"><span class="c0">Yes, the refund amount will be in process to your bank account in 3 working days.</span></p>
+<p class="c1 c2"><span class="c0"></span></p>
+<p class="c1"><span class="c0">Within how many days will my money will be deposited in my account?</span></p>
+<p class="c1"><span class="c0">Within 2 working days, your money will be deposited in your provided account</span></p>
+<p class="c1 c2"><span class="c0"></span></p>
+<p class="c1"><span class="c0">What if the brand gets inactive and does not end campaign?</span></p>
+<p class="c1"><span class="c0">After a request is raised to our support, we provide 10 days to brand to respond about the ongoing campaign, if the brand fails to respond within 10 days, we will automatically deduct amount from brands account to influencers account </span></p>
+<p class="c1 c2"><span class="c0"></span></p>
+<p class="c1"><span class="c0">What if a brand tries to collaborate outside the platform?</span></p>
+<p class="c1"><span class="c0">Beware of such brands as it may lead to fraudulently activities in which we may not be able to intervene. Such activities on our platform may lead to permanent ban.</span></p>
+<p class="c1 c2"><span class="c0"></span></p>
+<p class="c1 c2"><span class="c0"></span></p>
+      </div>`;
+
+var Faq = function () {
+  var Faq = {
+    text: FaqText,
+  };
+  return Faq;
+};
